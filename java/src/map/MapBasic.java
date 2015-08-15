7435a5952537bbd7e1884ac6445f7898eecaa498
Steven Cooks <stevenjoy1113@gmail.com>
2015-08-15 09:56:52 -0400

    add

--- java/src/map/MapBasic.java
+++ java/src/map/MapBasic.java
@@ -1,9 +1,14 @@
 package map;
 
+import java.util.Map;
+
 public class MapBasic {
     
     public static void main(String[] args) {
         
+    }
+    
+    public void loop(Map<String, Integer> employers) {
     }
 
 }
